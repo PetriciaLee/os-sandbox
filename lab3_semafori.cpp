eafafc6adcaee3fbd24d37d5dea764fad578fa64
Petra Jakopović <93216101+PetriciaLee@users.noreply.github.com>
2026-06-08 22:19:07 +0200

    Update lab3_semafori.cpp

--- lab3_semafori.cpp
+++ lab3_semafori.cpp
@@ -1,193 +1,120 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 #include <unistd.h>
-#include <cstdlib>
-#include <ctime>
-#include <vector>
-#include <string>
-#include <csignal>
 
-using namespace std;
+long long globalni_broj = 0;
 
-const int LINUX = 0;
-const int MICROSOFT = 1;
+sem_t sem_prazan; 
+sem_t sem_pun;    
 
-int N;            
-int broj_svake_vrste; 
-
-
-pthread_mutex_t monitor_latch; 
-pthread_cond_t red_linux;      
-pthread_cond_t red_microsoft;  
-
-int u_restoranu = 0;       
-int vrsta_u_restoranu = -1;
-int cekaju_L = 0;          
-int cekaju_M = 0;          
-
-int propusteno_L = 0;
-int propusteno_M = 0;
-
-void ispis_stanja(int id, int vrsta, string akcija) {
-    string ispis_L = "";
-    for (int i = 0; i < broj_svake_vrste; i++) {
-        if (i < cekaju_L) ispis_L += "L";
-        else ispis_L += "-";
-    }
-
-    string ispis_M = "";
-    for (int i = 0; i < broj_svake_vrste; i++) {
-        if (i < cekaju_M) ispis_M += "M";
-        else ispis_M += "-";
-    }
-
-    string ispis_restoran = "";
-    if (vrsta_u_restoranu != -1) {
-        char oznaka = (vrsta_u_restoranu == LINUX) ? 'L' : 'M';
-        for (int i = 0; i < u_restoranu; i++) {
-            ispis_restoran += oznaka;
-        }
-    }
-
-    char trenutni_p = (vrsta == LINUX) ? 'L' : 'M';
-    
-    cout << "Red Linux:" << ispis_L << " Red Microsoft:" << ispis_M 
-         << " Restoran:" << ispis_restoran << " --> " << trenutni_p << " " << akcija << endl;
-}
-
-void udji(int vrsta, int id) {
-    pthread_mutex_lock(&monitor_latch);
-
-    if (vrsta == LINUX) {
-        cekaju_L++;
-        while (vrsta_u_restoranu == MICROSOFT || (cekaju_M > 0 && propusteno_L >= N)) {
-            ispis_stanja(id, vrsta, "u red čekanja");
-            pthread_cond_wait(&red_linux, &monitor_latch);
-        }
-        cekaju_L--;
-        u_restoranu++;
-        vrsta_u_restoranu = LINUX;
-        propusteno_L++;
-        propusteno_M = 0; 
-        ispis_stanja(id, vrsta, "u restoran");
-    } 
-    else { 
-        cekaju_M++;
-        
-        while (vrsta_u_restoranu == LINUX || (cekaju_L > 0 && propusteno_M >= N)) {
-            ispis_stanja(id, vrsta, "u red čekanja");
-            pthread_cond_wait(&red_microsoft, &monitor_latch);
-        }
-        cekaju_M--;
-        u_restoranu++;
-        vrsta_u_restoranu = MICROSOFT;
-        propusteno_M++;
-        propusteno_L = 0; 
-        ispis_stanja(id, vrsta, "u restoran");
-    }
-
-    pthread_mutex_unlock(&monitor_latch);
-}
-
-void izadji(int vrsta, int id) {
-    pthread_mutex_lock(&monitor_latch);
-
-    u_restoranu--;
-    ispis_stanja(id, vrsta, "iz restorana");
-
-    if (u_restoranu == 0) {
-        vrsta_u_restoranu = -1; 
-
-        if (vrsta == LINUX) {
-            if (cekaju_M > 0 && (propusteno_L >= N || cekaju_L == 0)) {
-                pthread_cond_broadcast(&red_microsoft);
-            } else if (cekaju_L > 0) {
-                pthread_cond_broadcast(&red_linux);
-            } else if (cekaju_M > 0) {
-                pthread_cond_broadcast(&red_microsoft);
-            }
-        } 
-        else { 
-            if (cekaju_L > 0 && (propusteno_M >= N || cekaju_M == 0)) {
-                pthread_cond_broadcast(&red_linux);
-            } else if (cekaju_M > 0) {
-                pthread_cond_broadcast(&red_microsoft);
-            } else if (cekaju_L > 0) {
-                pthread_cond_broadcast(&red_linux);
-            }
-        }
-    }
-
-    pthread_mutex_unlock(&monitor_latch);
-}
-
-struct dretva_arg {
+typedef struct {
     int id;
-    int vrsta;
-};
+} dretva_podaci;
 
-void* programer(void* arg) {
-    dretva_arg* p_info = (dretva_arg*)arg;
-    int vrsta = p_info->vrsta;
-    int id = p_info->id;
+void* dretva_generiraj(void* arg) {
+    int* parametri = (int*)arg;
+    int ukupno_zadataka = parametri[0];
+    int broj_radnika = parametri[1];
 
-    usleep(rand() % 100 * 1000); 
+    printf("Dretva koja generira zadatke pocela je s radom. Broj zadataka=%d\n", ukupno_zadataka);
 
-   
-    udji(vrsta, id);
+    for (int i = 0; i < ukupno_zadataka; i++) {
+        sem_wait(&sem_prazan); 
 
-    usleep(rand() % 100 * 1000); 
+        globalni_broj = ((long long)rand() % 1000000000) + 1;
+        printf("Generiran broj %lld\n", globalni_broj);
+        
+        usleep(10000); 
 
-    izadji(vrsta, id);
+        sem_post(&sem_pun); 
+    }
 
-    delete p_info; 
+    for (int i = 0; i < broj_radnika; i++) {
+        sem_wait(&sem_prazan);
+        globalni_broj = -1;
+        sem_post(&sem_pun);
+    }
+
     return NULL;
 }
 
-void unisti_monitore() {
-    pthread_mutex_destroy(&monitor_latch);
-    pthread_cond_destroy(&red_linux);
-    pthread_cond_destroy(&red_microsoft);
-}
+void* dretva_racunaj(void* arg) {
+    int id = ((dretva_podaci*)arg)->id;
+    printf("Dretva %d pocela je s radom.\n", id);
 
-void ukloni_na_signal(int sig) {
-    unisti_monitore();
-    exit(0);
+    while (1) {
+        sem_wait(&sem_pun); 
+
+        long long lokalni_broj = globalni_broj;
+
+        if (lokalni_broj == -1) {
+            sem_post(&sem_prazan); 
+            break;
+        }
+
+        printf("Dretva %d. preuzela zadatak %lld\n", id, lokalni_broj);
+        
+        sem_post(&sem_prazan); 
+
+        unsigned long long zbroj = 0;
+        for (long long i = 1; i <= lokalni_broj; i++) {
+            zbroj += i;
+        }     
+        
+        printf("Dretva %d. zadatak=%lld zbroj=%llu\n", id, lokalni_broj, zbroj);
+    }
+    return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        cerr << "Pogreška! Ispravan unos: " << argv[0] << " <N_propustanje> <broj_programera_svake_vrste>" << endl;
+    if (argc != 3) {
+        printf("Uporaba: %s <broj_radnih_dretvi (m)> <broj_zadataka (n)>\n", argv[0]);
         return 1;
     }
 
-    N = atoi(argv[1]);
-    broj_svake_vrste = atoi(argv[2]);
+    int m = atoi(argv[1]); 
+    int n = atoi(argv[2]); 
+
+    if (m <= 0 || n <= 0) {
+        printf("Argumenti moraju biti pozitivni cijeli brojevi veći od nule.\n");
+        return 1;
+    }
 
     srand(time(NULL));
-    signal(SIGINT, ukloni_na_signal);
+    
+    sem_init(&sem_prazan, 0, 1); 
+    sem_init(&sem_pun, 0, 0);    
 
-    pthread_mutex_init(&monitor_latch, NULL);
-    pthread_cond_init(&red_linux, NULL);
-    pthread_cond_init(&red_microsoft, NULL);
+    pthread_t dretva_gen;
+    
+    pthread_t* radne_dretve = (pthread_t*)malloc(m * sizeof(pthread_t));
+    dretva_podaci* podaci = (dretva_podaci*)malloc(m * sizeof(dretva_podaci));
 
-    int ukupno_dretvi = broj_svake_vrste * 2;
-    vector<pthread_t> dretve(ukupno_dretvi);
-
-    int idx = 0;
-    for (int i = 0; i < broj_svake_vrste; i++) {
-        dretva_arg* argL = new dretva_arg{i, LINUX};
-        pthread_create(&dretve[idx++], NULL, programer, (void*)argL);
-
-        dretva_arg* argM = new dretva_arg{i, MICROSOFT};
-        pthread_create(&dretve[idx++], NULL, programer, (void*)argM);
+    if (radne_dretve == NULL || podaci == NULL) {
+        perror("Greška kod alokacije memorije");
+        return 1;
     }
 
-    for (int i = 0; i < ukupno_dretvi; i++) {
-        pthread_join(dretve[i], NULL);
+    int gen_parametri[2] = {n, m};
+    pthread_create(&dretva_gen, NULL, dretva_generiraj, gen_parametri);
+
+    for (int i = 0; i < m; i++) {
+        podaci[i].id = i + 1;
+        pthread_create(&radne_dretve[i], NULL, dretva_racunaj, &podaci[i]);
     }
 
-    unisti_monitore();
+    pthread_join(dretva_gen, NULL);
+    for (int i = 0; i < m; i++) {
+        pthread_join(radne_dretve[i], NULL);
+    }
+
+    sem_destroy(&sem_prazan);
+    sem_destroy(&sem_pun);
+    free(radne_dretve);
+    free(podaci);
 
     return 0;
 }
